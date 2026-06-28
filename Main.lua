5ad8eb4523f15e8f066214a5b5af6e75af4d89be
Raywans <ggpapa7777@gmail.com>
2026-06-27 17:31:44 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,31 @@
+local Config = {
+    -- [1] PlatoBoost Settings
+    ServiceId       = 27041, 
+    PlatoSecret     = "bcfbbd06-e449-411f-a489-8ce0605200d9", 
 
+    -- [2] Anti-Bypass / Global Secret Variable
+    Secret          = "Joserax_MVS_Secret_2026", 
+
+    -- [3] Scripts & Links
+    -- 👇 AQUÍ PONES EL ENLACE RAW DE TU SCRIPT OFUSCADO 👇
+    MainScriptURL   = "https://raw.githubusercontent.com/JOSERAX11/MVSDUELSSCRIPT/refs/heads/main/TU_SCRIPT_OFUSCADO.lua", 
+    
+    -- [4] Social Media Settings
+    ShowDiscord     = false,
+    DiscordURL      = "https://discord.gg/TuDiscordAqui",
+    ShowInstagram   = false,
+    InstagramURL    = "https://www.instagram.com/TuUsuario/",
+    ShowYoutube     = false,
+    YoutubeURL      = "https://www.youtube.com/channel/TuCanal",
+
+    -- [5] File System
+    KeyFileName     = "MVS_Duels_Key.txt", 
+
+    -- [6] GUI Management
+    OldGuiName      = "DASHBOARD_KeySystem", 
+    MainGuiName     = "🎯 External Aimbot Menu",
+
+    -- [7] Hub Information & UI Text
+    HubName         = "DASHBOARD-HUB", 
+    HubDescription  = "Script Oficial de MVS Duels - Verifica tu Key" 
+}
